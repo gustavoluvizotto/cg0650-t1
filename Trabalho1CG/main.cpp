3b574b0638df90c34a9fc6c02a047b6fd8a5879e
gilberto.vollpe@gmail.com <gilberto.vollpe@gmail.com@7b2fb127-79cd-0f5e-a8e3-67ac5057d8d0>
2013-03-22 19:58:02 +0000

    Create Project

---
+++ Trabalho1CG/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: gilvolpe
+ *
+ * Created on March 22, 2013, 5:00 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    cout << "Hello World" << endl;
+    
+    return 0;
+}
+
